0f8d9c76682e0e9f9f0cd2a3099c68f324660aad
dc00274 <dkcclayton@gmail.com>
2025-09-11 13:01:03 -0400

    Update the BillItem class
    added final to the name and amount variables

--- Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,6 +1,9 @@
 package edu.westga.cs1302.lab3.views;
 
 
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+
 /**
  * Controller class for drawing various things to our canvas window.
  * 
@@ -22,15 +25,33 @@ public class MainWindow {
 
 	    @FXML
 	    private TextArea output;
-
+	    
+        private Bill bill;
+        private BillView billView;
+        
 	    @FXML
 	    void addItem(ActionEvent event) {
+	    	try {
+	    		String billName = name.getText();
+	    		double billAmount = Double.parseDouble(billName);
+	    		BillItem item = new BillItem(billName, billAmount);
+	    		bill.addItem(item);
+	    		String billText = billView.getText(bill);
+	    		output.setText(billText);
+	    		
+	    	}catch (IllegalArgumentException e) {
+	    		output.setText(e.getMessage());
+	    	}
 	    }
 
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
-    	
+    	billView =  new BillView();
+    	bill = new Bill();
     }
+    
+    
+    
 }
