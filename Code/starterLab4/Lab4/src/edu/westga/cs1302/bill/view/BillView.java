cf745f64dffc81db28d9efd1e9bc0588ea1427ec
dc00274 <dkcclayton@gmail.com>
2025-09-15 00:57:36 -0400

    Update BillView
    updated the getText method  to make use of the new methods

--- Code/starterLab4/Lab4/src/edu/westga/cs1302/bill/view/BillView.java
+++ Code/starterLab4/Lab4/src/edu/westga/cs1302/bill/view/BillView.java
@@ -1,6 +1,9 @@
 package edu.westga.cs1302.bill.view;
 
+import java.util.ArrayList;
+
 import edu.westga.cs1302.bill.model.Bill;
+import edu.westga.cs1302.bill.model.BillCalculator;
 import edu.westga.cs1302.bill.model.BillItem;
 
 /** Supports displaying the information contained in a Bill.
@@ -21,16 +24,20 @@ public class BillView {
 	 */
 	public static String getText(Bill bill) {
 		String text = "ITEMS" + System.lineSeparator();
+		ArrayList<BillItem> itemList = bill.getItems();
+		BillItem[] items = itemList.toArray(new BillItem[0]);
 		double subTotal = 0.0;
 		for (BillItem item : bill.getItems()) {
 			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
 			subTotal += item.getAmount();
 		}
 		
+		double subtotal = BillCalculator.calculateSubtotal(items);
+        double tip = BillCalculator.calculateTax(items);
+        double tax = BillCalculator.calculateTip(items);
+        double total = BillCalculator.calculateTotal(items);
 		text += System.lineSeparator();
 		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
-		double tax = subTotal * Bill.TAX_RATE;
-		double tip = subTotal * Bill.TIP_RATE;
 		text += "TAX - $" + BillView.roundToNearestHundredth(tax) + System.lineSeparator();
 		text += "TIP - $" + BillView.roundToNearestHundredth(tip) + System.lineSeparator();
 		text += "TOTAL - $" + BillView.roundToNearestHundredth(subTotal + tip + tax);
