6d96815eda89cf495915ea25ad9622f106816aac
dc00274 <dkcclayton@gmail.com>
2025-12-09 01:03:38 -0500

    Model for adding comics to a collection
    added a data class that stores a comic and updated collection class.

--- Code/L20-SampleSystem/Lecture 20 - Sample System/src/main/java/edu/westga/cs1302/coursegui/model/CollectionItem.java
+++ Code/L20-SampleSystem/Lecture 20 - Sample System/src/main/java/edu/westga/cs1302/coursegui/model/CollectionItem.java
@@ -1,19 +1,38 @@
 package edu.westga.cs1302.coursegui.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class CollectionItem {
 	private final String name;
-	
+	private final ObservableList<Comic> comics;
 	
 	public CollectionItem(String name) {
         if (name == null || name.isBlank())
             throw new IllegalArgumentException("Collection name cannot be empty.");
         this.name = getName();
+        this.comics = FXCollections.observableArrayList();
     }
 	
 	public String getName() {
         return name;
     }
-
+	
+	public ObservableList<Comic> getComics(){
+		return comics;
+	}
+    
+	public void addComic(Comic comic) {
+		if(!comics.contains(comic)) {
+			comics.add(comic);
+		}
+	}
+	
+	public void removeComic(Comic comic) {
+		comics.remove(comic);
+	}
+	
+	
     @Override
     public String toString() {
         return name;
---
+++ Code/L20-SampleSystem/Lecture 20 - Sample System/src/main/java/edu/westga/cs1302/coursegui/model/Comic.java
@@ -0,0 +1,5 @@
+package edu.westga.cs1302.coursegui.model;
+
+public class Comic {
+
+}
