fbc0047c4354d2a5a53503affa73a43aa05a50cf
dc00274 <dkcclayton@gmail.com>
2025-12-09 01:36:26 -0500

    Viewmodel for adding comic to collection
    properties for title,issuenumber,list of comics and storing select comics
    added and corresponding methods.

--- Code/L20-SampleSystem/Lecture 20 - Sample System/src/main/java/edu/westga/cs1302/coursegui/viewmodel/CollectionViewModel.java
+++ Code/L20-SampleSystem/Lecture 20 - Sample System/src/main/java/edu/westga/cs1302/coursegui/viewmodel/CollectionViewModel.java
@@ -1,7 +1,12 @@
 package edu.westga.cs1302.coursegui.viewmodel;
 
+import java.util.Collection;
+
 import edu.westga.cs1302.coursegui.model.CollectionItem;
+import edu.westga.cs1302.coursegui.model.Comic;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -12,7 +17,8 @@ public class CollectionViewModel {
 	private final StringProperty collectionName = new SimpleStringProperty("");
 	 private final ObservableList<CollectionItem> collections = FXCollections.observableArrayList();
 	 private final ObjectProperty<CollectionItem> selectedCollection = new SimpleObjectProperty<>(null);
-	 
+	 private final StringProperty comicTitle = new SimpleStringProperty(" ");
+	 private final IntegerProperty issueNumber = new SimpleIntegerProperty(0);
 	 
 	 public StringProperty newCollectionNameProperty() {
 	        return collectionName;
@@ -25,33 +31,48 @@ public class CollectionViewModel {
 	    public ObjectProperty<CollectionItem> selectedCollectionProperty() {
 	        return selectedCollection;
 	    }
+	    public StringProperty comicTitleProperty() {
+	    	return  comicTitle;
+	    }
+	    
+	    public IntegerProperty issueNumberProperty() {
+	    	return issueNumber;
+	    }
 	    
 	    public boolean addCollection() {
-	        String name = collectionName.get().trim();
+	        if(selectedCollection.get() == null) {
+	        	return false;
+	        }
 
-	        if (name.isEmpty())
+	        String title = comicTitle.get();
+	        int comicIssueNumber = issueNumber.get();
+	        if (title.isEmpty() || issueNumber <= 0) {
 	            return false;
+	        }
 
-	        
-	        boolean exists = collections.stream()
-	                .anyMatch(c -> c.getName().equalsIgnoreCase(name));
-
-	        if (exists)
-	            return false;
-
-	        collections.add(new CollectionItem(name));
-	        collectionName.set(""); 
+	        Comic newComic = new Comic(title, issueNumber);
+	        selectedCollection.get().addComic(newComic);
+	        comicsInSelectedCollection.setAll(selectedCollection.get().getComics());
+            comicTitle.set(" ");
+            issueNumber.set(0);
 	        return true;
 	    }
 	    
 	    public boolean removeSelectedCollection() {
-	        CollectionItem selected = selectedCollection.get();
-
-	        if (selected == null)
+	    	if (selectedCollection.get() == null || selectedComic.get() == null) {
 	            return false;
+	        }
 
-	        collections.remove(selected);
-	        selectedCollection.set(null);
+	        selectedCollection.get().removeComic(selectedComic.get());
+	        comicsInSelectedCollection.setAll(selectedCollection.get().getComics());
+	        selectedComic.set(null);
+
 	        return true;
 	    }
+	    
+	    public void setSelectedCollection(Collection collection) {
+	        selectedCollection.set(collection);
+	        comicsInSelectedCollection.setAll(collection.getComics());
+	    }
+	    
 }
